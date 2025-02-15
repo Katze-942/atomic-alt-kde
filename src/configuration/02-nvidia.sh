feb8338d97270abc029ac2a6db445ab5917ae071
Жора Змейкин <rs5334819@gmail.com>
2025-02-16 01:54:15 +0400

    Попытка исправить ссылки на модули ядра при установке NVIDIA драйвера

--- src/configuration/02-nvidia.sh
+++ src/configuration/02-nvidia.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+set -e
+
 echo "::group:: ===$(basename "$0")==="
 
-set -e
+ls /
+ls /lib
+ls /var/lib/modules
 
 wget "https://ru.download.nvidia.com/XFree86/Linux-x86_64/570.86.16/NVIDIA-Linux-x86_64-570.86.16.run"
 chmod +x ./NVIDIA-Linux-x86_64-570.86.16.run
-./NVIDIA-Linux-x86_64-570.86.16.run --ui=none -q --dkms --rebuild-initramfs --kernel-source-path=/lib/modules/$(uname -r)/build
+./NVIDIA-Linux-x86_64-570.86.16.run --ui=none -q --dkms --rebuild-initramfs --kernel-source-path=/var/lib/modules/$(uname -r)/build
 rm NVIDIA-Linux-x86_64-570.86.16.run
 
 touch /usr/lib/bootc/kargs.d/10-nvidia.toml
