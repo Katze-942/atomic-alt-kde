db46802249d4274fa84eea90f835aa7e29303b84
Жора Змейкин <rs5334819@gmail.com>
2025-02-16 02:05:28 +0400

    fix build nvidia driver

--- src/configuration/02-nvidia.sh
+++ src/configuration/02-nvidia.sh
@@ -4,13 +4,9 @@ set -e
 
 echo "::group:: ===$(basename "$0")==="
 
-ls /
-ls /lib
-ls /var/lib/modules
-
 wget "https://ru.download.nvidia.com/XFree86/Linux-x86_64/570.86.16/NVIDIA-Linux-x86_64-570.86.16.run"
 chmod +x ./NVIDIA-Linux-x86_64-570.86.16.run
-./NVIDIA-Linux-x86_64-570.86.16.run --ui=none -q --dkms --rebuild-initramfs --kernel-source-path=/var/lib/modules/$(uname -r)/build
+./NVIDIA-Linux-x86_64-570.86.16.run --ui=none -q --dkms --rebuild-initramfs --kernel-source-path=/lib/modules/$(ls /lib/modules/ | tail -n 1)/build
 rm NVIDIA-Linux-x86_64-570.86.16.run
 
 touch /usr/lib/bootc/kargs.d/10-nvidia.toml
