3b32b50f819bb9fdeeb7b376c9ddfdb3b1b4cfc5
Жора Змейкин <48821354+Katze-942@users.noreply.github.com>
2025-02-17 00:35:27 +0400

    Update 02-nvidia.sh

--- src/configuration/02-nvidia.sh
+++ src/configuration/02-nvidia.sh
@@ -6,7 +6,7 @@ echo "::group:: ===$(basename "$0")==="
 
 apt-get install -y apt-repo-lakostis-kmodules apt-repo-lakostis && \
   apt-get update && \
-  apt-get install to nvidia-smi kernel-modules-nvidia-6.12#3:570.86.16-alt1.396301.1@1739197780 nvidia_glx libnvoptix
+  apt-get install nvidia-smi kernel-modules-nvidia-6.12#3:570.86.16-alt1.396301.1@1739197780 nvidia_glx libnvoptix
 
 
 touch /usr/lib/bootc/kargs.d/10-nvidia.toml
