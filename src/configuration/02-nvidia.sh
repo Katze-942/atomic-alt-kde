39d904b38a40c25f612738dd3191a5f393a1ac6a
Жора Змейкин <48821354+Katze-942@users.noreply.github.com>
2025-02-16 23:52:33 +0400

    Update 02-nvidia.sh

--- src/configuration/02-nvidia.sh
+++ src/configuration/02-nvidia.sh
@@ -4,10 +4,10 @@ set -e
 
 echo "::group:: ===$(basename "$0")==="
 
-wget "https://ru.download.nvidia.com/XFree86/Linux-x86_64/570.86.16/NVIDIA-Linux-x86_64-570.86.16.run"
-chmod +x ./NVIDIA-Linux-x86_64-570.86.16.run
-./NVIDIA-Linux-x86_64-570.86.16.run --ui=none -q --dkms --rebuild-initramfs --kernel-source-path=/lib/modules/$(ls /lib/modules/ | tail -n 1)/build
-rm NVIDIA-Linux-x86_64-570.86.16.run
+apt-get install -y apt-repo-lakostis-kmodules apt-repo-lakostis && \
+  apt-get update && \
+  apt-get install to nvidia-smi kernel-modules-nvidia-6.12#3:570.86.16-alt1.396301.1@1739197780 nvidia_glx libnvoptix
+
 
 touch /usr/lib/bootc/kargs.d/10-nvidia.toml
 echo "kargs = [\"nvidia_drm.modeset=1\"]" > /usr/lib/bootc/kargs.d/10-nvidia.toml
