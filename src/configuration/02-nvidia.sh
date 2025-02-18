33a1bb7ee2aa91e9ee37b921d315a76576ef41ec
Жора Змейкин <48821354+Katze-942@users.noreply.github.com>
2025-02-18 14:03:56 +0400

    Remove NVIDIA

--- src/configuration/02-nvidia.sh
+++ src/configuration/02-nvidia.sh
@@ -4,11 +4,6 @@ set -e
 
 echo "::group:: ===$(basename "$0")==="
 
-apt-get update
-apt-get install -y apt-repo-lakostis-kmodules apt-repo-lakostis
-apt-get update
-apt-get install -y nvidia-smi kernel-modules-nvidia-6.12#3:570.86.16-alt1.396301.1@1739197780 nvidia_glx libnvoptix
-
 touch /usr/lib/bootc/kargs.d/10-nvidia.toml
 echo "kargs = [\"nvidia_drm.modeset=1\"]" > /usr/lib/bootc/kargs.d/10-nvidia.toml
 
