e4f8418a74de84dabf4192cd0fc5a1ac889b8619
Жора Змейкин <48821354+Katze-942@users.noreply.github.com>
2025-03-15 02:47:49 +0400

    Update 02-nvidia.sh

--- src/configuration/02-nvidia.sh
+++ src/configuration/02-nvidia.sh
@@ -4,10 +4,10 @@ set -e
 
 echo "::group:: ===$(basename "$0")==="
 
-wget "https://ru.download.nvidia.com/XFree86/Linux-x86_64/570.86.16/NVIDIA-Linux-x86_64-570.86.16.run"
-chmod +x ./NVIDIA-Linux-x86_64-570.86.16.run
-./NVIDIA-Linux-x86_64-570.86.16.run --ui=none -q --dkms --kernel-source-path=/lib/modules/$(ls /lib/modules/ | tail -n 1)/build
-rm NVIDIA-Linux-x86_64-570.86.16.run
-depmod -a -v "$(ls /lib/modules/ | tail -n 1)"
+#wget "https://ru.download.nvidia.com/XFree86/Linux-x86_64/570.86.16/NVIDIA-Linux-x86_64-570.86.16.run"
+# chmod +x ./NVIDIA-Linux-x86_64-570.86.16.run
+# ./NVIDIA-Linux-x86_64-570.86.16.run --ui=none -q --dkms --kernel-source-path=/lib/modules/$(ls /lib/modules/ | tail -n 1)/build
+# rm NVIDIA-Linux-x86_64-570.86.16.run
+# depmod -a -v "$(ls /lib/modules/ | tail -n 1)"
 
 echo "End 02-nvidia.sh"
